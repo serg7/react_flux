50894b070f23ed0c5e1133341e99a6e3419524e8
Sergey <serhiyborsuk@gmail.com>
2017-02-18 10:21:37 +0200

    add react, bootstrap,, jquery & eslint

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ var gulp = require('gulp'),
     open = require('gulp-open'),  // Open a URL in a browser
     browserify = require('browserify'),
     reactify = require('reactify'),  // transforms React JSX to JS
-    source = require('vinyl-source-stream');
+    source = require('vinyl-source-stream'),
+    concat = require('gulp-concat'),
+    lint = require('gulp-eslint');  // Lints JS and JSX files
 
 var config = {
     port: 9005,
@@ -13,6 +15,10 @@ var config = {
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
         dist: './dist',
         mainJs: './src/main.js'
     }
@@ -48,9 +54,21 @@ gulp.task('js', function () {
         .pipe(connect.reload());
 });
 
-gulp.task('watch', function () {
-    gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.js, ['js']);
+gulp.task('css', function() {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'))
 });
 
-gulp.task('default', ['html', 'js', 'open', 'watch']);
+gulp.task('lint', function () {
+    return gulp.src(config.paths.js)
+        .pipe(lint()) //{config: './eslint.config.json'}
+        .pipe(lint.format());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 
+$ = jQuery = require('jquery');
+
+test = 1;
+
 var App = console.log('Hi from Browserify');
 
 module.exports = App;
