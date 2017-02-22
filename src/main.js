9df6f30b651d5f650b06ae023e4cedeb78b7d50c
Sergey <serhiyborsuk@gmail.com>
2017-02-22 17:54:24 +0200

    remove unused files

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ var Router = require('react-router');
 var routes = require('./routes');
 
 Router.run(routes, function(Handler) {
-    React.render(<Handler/>, document.getElementById('app'));
+	React.render(<Handler/>, document.getElementById('app'));
 });
